1e5e509ee148d7fac669b2cbc08e4521b55715d5
Mateusz Jabłoński <mateusz.jablonski@all-for-one.com>
2022-05-20 11:32:43 +0200

    part 2

--- a41-blog/components/Breadcrumbs/index.tsx
+++ a41-blog/components/Breadcrumbs/index.tsx
@@ -1,11 +1,21 @@
+import { useRouter } from "next/router";
+import Link from 'next/link'
+
 const Breadcrumbs = () => {
+    const { pathname } = useRouter();
+
+    const getPath = () => pathname.split('/')[1];
+
     return <nav className="breadcrumb" aria-label="breadcrumbs">
         <ul>
             <li>Home</li>
-            {/* dokończ */}
+            <li>
+                <Link href={getPath()}>
+                    <a>{getPath().toUpperCase()}</a>
+                </Link>
+            </li>
         </ul>
     </nav>
-    
 }
 
 export default Breadcrumbs;
